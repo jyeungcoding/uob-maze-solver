f84fd15be0d24167d737fc376f3a2f1b73f65afd
Justin Yeung <justinyeung@Justins-MacBook-Pro.local>
2022-02-15 17:47:07 +0000

    Added more design notes to consider.

--- image_detection/image_detection.py
+++ image_detection/image_detection.py
@@ -10,7 +10,12 @@ def initialise_maze():
     '''
     This function is run once at the beginning of the program. It captures all elements in the
     maze (ball, walls, holes and checkpoints) and outputs a single Maze object as defined in
-    'objects.py'. Please check 'objects.py' for the required specifications.
+    'objects.py'. Please check 'objects.py' for the required specifications. Please remember
+    that the y axis starts at the top left corner and increases as you go down the maze,
+    opposite to a traditional coordinate system. Furthermore, the side frames of the maze are
+    included in the coordiate system so the "true" area where maze objects can exists should
+    be from (28.75, 28.25) to (303.25, 257.75). Do check this based on the frame and maze
+    sizes in the settings file though. 
     '''
 
     return Maze
@@ -21,7 +26,7 @@ def update_ball():
     our maze objects. The output should be in the format of a list: [Active, Position].
     Active should be a boolean value and should be set to True as long as the ball is still
     on the maze, and False when the ball has fallen through a hole. The Position of the Ball
-    should be provided as np.array([x, y]). See objects.py for more information. 
+    should be provided as np.array([x, y]). See objects.py for more information.
     '''
 
     return Output
