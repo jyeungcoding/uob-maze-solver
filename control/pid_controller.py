5b13ab4ed5ff9cfbbf09196c364bbfa609e62e7e
Justin Yeung <justinyeung@eduroam-171-55.nomadic.bris.ac.uk>
2022-02-22 16:19:13 +0000

    Moved all PID control memory elements and functions to a PID controller class in control/pid_controller.py.

---
+++ control/pid_controller.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+'''
+This file contains a class for the PID controller system, including all memory elements
+and functions needed to calculate the control signal. Initialise the class with the chosen
+settings and the initial set point. Change the set point with new_setpoint(SetPoint); this
+also resets the memory elements. update(ProcessVariable, TimeStep) outputs the control signal.
+'''
+
+# Import modules.
+import numpy as np
+
+class PID_Controller():
+
+    def __init__(self, Kp, Ki, Kd, SetPoint, BufferSize, SaturationLimit):
+        self.Kp = Kp # Proportional coefficient.
+        self.Ki = Ki # Integral coefficient.
+        self.Kd = Kd # Derivative coefficient.
+        self.SetPoint = SetPoint # Current set point.
+        self.BufferSize = BufferSize # Number of error values to store in the buffer.
+        self.ErrorBuffer = np.zeros((9, self.BufferSize)) # Initialise error buffer (with additional rows for linear regression).
+        self.BufferIteration = 0 # Record buffer iteration number.
+        self.ErrorIntegral = np.array([0.0, 0.0]) # Initialise integrator.
+        self.Saturation = np.array([False, False]) # Initialise saturation check.
+        self.SaturationLimit = SaturationLimit # Control signal angle limit.
+
+    def __repr__(self):
+        # Makes the class printable.
+        return "PID Controller(Kp: %s, Ki: %s, Kd: %s, Set Point: %s)" % (self.Kp, self.Ki, self.Kd, self.SetPoint)
+
+    def new_setpoint(self, SetPoint):
+        self.SetPoint = SetPoint # Set new set point.
+        self.ErrorIntegral = np.array([0.0, 0.0]) # Reset error integral.
+        self.ErrorBuffer = np.zeros((9, self.BufferSize)) # Reset error buffer.
+        self.BufferIteration = 0 # Reset buffer iteration number.
+
+    def error_buffer(self, ErrorValue, TimeStep):
+        # Shifts error buffer by one and adds new error and timestep values.
+        self.ErrorBuffer[0:3] = np.roll(self.ErrorBuffer[0:3], -1, 1) # Shifts error buffer to the left.
+        self.ErrorBuffer[0:3, self.BufferSize - 1] = TimeStep + self.ErrorBuffer[0, self.BufferSize - 2], ErrorValue[0], ErrorValue[1] # Update rightmost values.
+        self.BufferIteration += 1 # Update buffer iteration number.
+
+    def conditional_integrator(self, ErrorValue, TimeStep):
+        # Conditional integrator, clamps if ControlSignal is saturated and the error is the same sign as the integral.
+        if np.all(self.Saturation) and np.sign(ErrorValue)[0] == np.sign(self.ErrorIntegral)[0] and np.sign(ErrorValue)[1] == np.sign(self.ErrorIntegral)[1]:
+            pass # X and Y clamped.
+        elif self.Saturation[0] == False and self.Saturation[1] == True and np.sign(ErrorValue)[1] == np.sign(self.ErrorIntegral)[1]:
+            self.ErrorIntegral[0] += ErrorValue[0] * TimeStep # Y clamped.
+        elif self.Saturation[1] == True and self.Saturation[0] == False and np.sign(ErrorValue)[0] == np.sign(self.ErrorIntegral)[0]:
+            self.ErrorIntegral[1] += ErrorValue[1] * TimeStep # X clamped.
+        else:
+            self.ErrorIntegral += ErrorValue * TimeStep # Both unclamped.
+        return self.ErrorIntegral
+
+    def linear_regression(self):
+        # Calculate derivative of error value using least squares linear regression method from a buffer of error value vs time.
+        if self.BufferIteration >= self.BufferSize: # Only begin when buffer is full.
+            MeanT = np.mean(self.ErrorBuffer[0]) # Calculate mean of T.
+            MeanX = np.mean(self.ErrorBuffer[1]) # Calculate mean of X.
+            MeanY = np.mean(self.ErrorBuffer[2]) # Calculate mean of Y.
+            self.ErrorBuffer[3] = tuple(map(lambda T : T - MeanT, self.ErrorBuffer[0])) # Calculate T - MeanT.
+            self.ErrorBuffer[4] = tuple(map(lambda x : x - MeanX, self.ErrorBuffer[1])) # Calculate x - MeanX.
+            self.ErrorBuffer[5] = tuple(map(lambda y : y - MeanY, self.ErrorBuffer[2])) # Calculate y - MeanY.
+            self.ErrorBuffer[6] = tuple(map(lambda T_MeanT, x_MeanX: T_MeanT * x_MeanX, self.ErrorBuffer[3], self.ErrorBuffer[4])) # Calculate T_MeanT * x_MeanX.
+            self.ErrorBuffer[7] = tuple(map(lambda T_MeanT, y_MeanY: T_MeanT * y_MeanY, self.ErrorBuffer[3], self.ErrorBuffer[5])) # Calculate T_MeanT * y_MeanY.
+            self.ErrorBuffer[8] = tuple(map(lambda T_MeanT: T_MeanT ** 2, self.ErrorBuffer[3])) # Calculate T_MeanT ^ 2.
+
+            GradX = np.sum(self.ErrorBuffer[6]) / np.sum(self.ErrorBuffer[8]) # Gradient of x = (T_MeanT * x_MeanX) / (T_MeanT ^ 2).
+            GradY = np.sum(self.ErrorBuffer[7]) / np.sum(self.ErrorBuffer[8]) # Gradient of y = (T_MeanT * y_MeanY) / (T_MeanT ^ 2).
+            ErrorDerivative = np.array([GradX, GradY])
+        else:
+            ErrorDerivative = np.array([0.0, 0.0])
+        return ErrorDerivative
+
+    def saturation_clamp(self, ControlSignal):
+        # Saturation clamp. Limits the ControlSignal to the SaturationLimit and records if saturation has occured.
+        if ControlSignal[0] > self.SaturationLimit:
+            ControlSignal[0] = self.SaturationLimit
+            self.Saturation[0] = True
+        elif ControlSignal[0] < -self.SaturationLimit:
+            ControlSignal[0] = -self.SaturationLimit
+            self.Saturation[0] = True
+        else:
+            self.Saturation[0] = False
+        if ControlSignal[1] > self.SaturationLimit:
+            ControlSignal[1] = self.SaturationLimit
+            self.Saturation[1] = True
+        elif ControlSignal[1] < -self.SaturationLimit:
+            ControlSignal[1] = -self.SaturationLimit
+            self.Saturation[1] = True
+        else:
+            self.Saturation[1] = False
+        return ControlSignal
+
+    def update(self, ProcessVariable, TimeStep):
+        ErrorValue = self.SetPoint - ProcessVariable # Calculate error value.
+        self.error_buffer(ErrorValue, TimeStep) # Update buffer.
+
+        ErrorIntegral = self.conditional_integrator(ErrorValue, TimeStep) # Calculate integral value.
+        ErrorDerivative = self.linear_regression() # Calculate derivative value.
+
+        # Calculate PID terms and control signal.
+        ProportionalTerm = self.Kp * ErrorValue
+        IntegralTerm = self.Ki * ErrorIntegral
+        DerivativeTerm = self.Kd * ErrorDerivative
+        ControlSignal = ProportionalTerm + IntegralTerm + DerivativeTerm # Calculate control signal.
+        ControlSignal = self.saturation_clamp(ControlSignal) # Apply saturation clamp if necessary.
+
+        return ControlSignal, ProportionalTerm, IntegralTerm, DerivativeTerm
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
--- simulation/PID_sim.py
+++ simulation/PID_sim.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+'''
+
+'''
 
 # Import modules.
 import pygame
@@ -10,12 +13,13 @@ from math import pi
 from objects import Maze
 from graphics.objects import SpriteBall, SpriteSetPoint
 from graphics.graphics import initialise_walls, initialise_holes, initialise_checkpoints
-from simulation.objects import SandboxMaze, SimpleMaze
-from settings import PixelScale, White, Black, ThetaMax, BufferSize
+from simulation.objects import SandboxMaze, SimpleMaze, CircleMaze
+from control.pid_controller import PID_Controller
+from settings import PixelScale, White, Black, Kp, Ki, Kd, BufferSize, SaturationLimit
 
 def PID_sim():
     # Generate starting maze.
-    ActiveMaze = SandboxMaze
+    ActiveMaze = SimpleMaze
 
     # Check MazeModel is correct type.
     if type(ActiveMaze) != Maze:
@@ -26,7 +30,7 @@ def PID_sim():
     pygame.init()
     # Initialise display surface.
     Screen = pygame.display.set_mode((ActiveMaze.Size[0] * PixelScale, (ActiveMaze.Size[1] + 42) * PixelScale))
-    pygame.display.set_caption("Maze Simulation")
+    pygame.display.set_caption("PID Simulation")
 
     # Initialise text module.
     pygame.font.init()
@@ -52,19 +56,8 @@ def PID_sim():
     ''' PYGAME GRAPHICS END '''
 
     ''' INITIALISE PID CONTROL '''
-    # PID Coefficients
-    Kp = 2.99e-3
-    Ki = 1e-4
-    Kd = 1e-3
-
-    # Initialise a few variables.
-    ProcessVariable = ActiveMaze.Ball.S # Set ProcessVariable as the ball's position.
-    SetPoint = ActiveMaze.Checkpoints[0].S # Set SetPoint as the first checkpoint.
-    ErrorValue = SetPoint - ProcessVariable # Calculate initial error value.
-    ErrorIntegral = np.array([0.0, 0.0]) # Initialise integrator.
-    Saturation = np.array([False, False]) # Initialise saturation check.
-    LeastSquare = np.zeros((9, BufferSize)) # Initialise LeastSquare error buffer.
-    BufferIteration = 0 # Record buffer iteration number.
+    # Initialise PID controller object, see control/pid_controller.py for more information.
+    PID_Controller1 = PID_Controller(Kp, Ki, Kd, ActiveMaze.Checkpoints[0].S, BufferSize, SaturationLimit)
     ''' INITIALISE PID CONTROL '''
 
     # Theta (radians) should be a size 2 vector of floats.
@@ -75,89 +68,29 @@ def PID_sim():
     # Start clock for time-steps.
     CurrentTime = time.perf_counter() # time.perf_counter() is more accurate but takes more processing time.
     StartTime = CurrentTime # Record start time, currently unused.
-    TimeStep = 0.001 # First timestep: value unimportant, but has to be small and non-zero.
-    i = 1
+    TimeStep = 0
     while Running == 1:
 
         # Simulate next step of maze using theta and a given timestep.
         Output = ActiveMaze.next_step(TimeStep, Theta) # Time step given in s.
 
         ''' PID CONTROL START '''
-        # Set ProcessVariable as the ball's position.
-        ProcessVariable = Output[1]
+        if Output[0] == True:
+            # Set ProcessVariable as the ball's position.
+            ProcessVariable = Output[1]
 
-        # If the ball is within 2mm of the set point, delete the current checkpoint and set the new first checkpoint as the set point.
-        while ((SetPoint[0] - ProcessVariable[0]) ** 2 + (SetPoint[1] - ProcessVariable[1]) ** 2) ** 0.5 < 2 and len(ActiveMaze.Checkpoints) > 1:
-            ActiveMaze.Checkpoints.pop(0)
-            SetPoint = ActiveMaze.Checkpoints[0].S
-            ErrorValue = SetPoint - ProcessVariable # Reset error value.
-            ErrorIntegral = np.array([0.0, 0.0]) # Reset error integral.
-            LeastSquare = np.zeros((9, BufferSize)) # Initialise LeastSquare error buffer.
-            BufferIteration = 0 # Reset buffer iteration number.
+            # If the ball is within 2mm of the set point, delete the current checkpoint and set the new first checkpoint as the set point.
+            while ((ActiveMaze.Checkpoints[0].S[0] - ProcessVariable[0]) ** 2 + (ActiveMaze.Checkpoints[0].S[1] - ProcessVariable[1]) ** 2) ** 0.5 < 2 and len(ActiveMaze.Checkpoints) > 1:
+                ActiveMaze.Checkpoints.pop(0)
+                PID_Controller1.new_setpoint(ActiveMaze.Checkpoints[0].S)
 
-        # Calculate error value.
-        ErrorValue = SetPoint - ProcessVariable
+            # Calculate control signal using the PID controller.
+            PID_Output = PID_Controller1.update(ProcessVariable, TimeStep)
+            Theta = PID_Output[0]
 
-        # Update error buffer.
-        LeastSquare[0:3] = np.roll(LeastSquare[0:3], -1, 1)
-        LeastSquare[0:3, BufferSize - 1] = TimeStep + LeastSquare[0, BufferSize - 2], ErrorValue[0], ErrorValue[1]
-        BufferIteration += 1 # Update buffer iteration number.
-
-        # Conditional integrator, clamps if ControlSignal is saturated and the error is the same sign as the output.
-        if Saturation[0] == True and Saturation[1] == True and np.sign(ErrorValue)[0] == np.sign(Theta)[0] and np.sign(ErrorValue)[1] == np.sign(Theta)[1]:
-            pass
-        elif Saturation[0] == False and Saturation[1] == True and np.sign(ErrorValue)[1] == np.sign(Theta)[1]:
-            ErrorIntegral[0] += ErrorValue[0] * TimeStep
-        elif Saturation[1] == True and Saturation[0] == False and np.sign(ErrorValue)[0] == np.sign(Theta)[0]:
-            ErrorIntegral[1] += ErrorValue[1] * TimeStep
-        else:
-            ErrorIntegral += ErrorValue * TimeStep
-
-        # Calculate derivative of error value.
-
-        if BufferIteration > 10:
-            MeanT = np.mean(LeastSquare[0])
-            MeanX = np.mean(LeastSquare[1])
-            MeanY = np.mean(LeastSquare[2])
-            LeastSquare[3] = tuple(map(lambda T : T - MeanT, LeastSquare[0]))
-            LeastSquare[4] = tuple(map(lambda x : x - MeanX, LeastSquare[1]))
-            LeastSquare[5] = tuple(map(lambda y : y - MeanY, LeastSquare[2]))
-            LeastSquare[6] = tuple(map(lambda T_MeanT, x_MeanX: T_MeanT * x_MeanX, LeastSquare[3], LeastSquare[4]))
-            LeastSquare[7] = tuple(map(lambda T_MeanT, y_MeanY: T_MeanT * y_MeanY, LeastSquare[3], LeastSquare[5]))
-            LeastSquare[8] = tuple(map(lambda T_MeanT: T_MeanT ** 2, LeastSquare[3]))
-
-            GradX = np.sum(LeastSquare[6]) / np.sum(LeastSquare[8])
-            GradY = np.sum(LeastSquare[7]) / np.sum(LeastSquare[8])
-            ErrorDerivative = np.array([GradX, GradY])
-        else:
-            ErrorDerivative = np.array([0.0, 0.0])
-
-        # Calculate PID Terms.
-        ProportionalTerm = Kp * ErrorValue
-        IntegralTerm = Ki * ErrorIntegral
-        DerivativeTerm = Kd * ErrorDerivative
-        # Calculate ControlSignal with PID equation.
-        ControlSignal = ProportionalTerm + IntegralTerm + DerivativeTerm
-
-        # Saturation clamp.
-        Theta = ControlSignal
-        Saturation = np.array([False, False])
-        if Theta[0] > ThetaMax: # If Theta exceeds ThetaMax clamp the integrator and output ThetaMax.
-            Theta[0] = ThetaMax
-            Saturation[0] = True
-        elif Theta[0] < -ThetaMax:
-            Theta[0] = -ThetaMax
-            Saturation[0] = True
-        if Theta[1] > ThetaMax:
-            Theta[1] = ThetaMax
-            Saturation[1] = True
-        elif Theta[1] < -ThetaMax:
-            Theta[1] = -ThetaMax
-            Saturation[1] = True
-
-        # Impose sanity check on Theta, raise error if greater than 0.25pi.
-        if Theta[0] > ThetaMax or Theta[0] < -ThetaMax or Theta[1] > ThetaMax or Theta[1] < -ThetaMax:
-            raise Exception("Control signal exceeded 0.25*pi in one or both axes. PLease check PID settings.")
+            # Theta sanity check: raise error if greater than 0.25pi.
+            if Theta[0] > SaturationLimit or Theta[0] < -SaturationLimit or Theta[1] > SaturationLimit or Theta[1] < -SaturationLimit:
+                raise Exception("Control signal exceeded SaturationLimit in one or both axes. PLease check PID settings.")
         ''' PID CONTROL END'''
 
         ''' PYGAME GRAPHICS START '''
@@ -181,9 +114,9 @@ def PID_sim():
         # Create surface with text describing the ball's position.
         BallPositionTxt = Font1.render(str(ActiveMaze.Ball), False, Black)
         # Create surface with text describing the PID Terms.
-        PIDTermsTxt = Font1.render("P: %s, I: %s, D: %s" % (np.round(ProportionalTerm * 360 / (2 * pi), 1), np.round(IntegralTerm * 360 / (2 * pi), 1), np.round(DerivativeTerm * 360 / (2 * pi), 1)), False, Black)
+        PIDTermsTxt = Font1.render("P: %s, I: %s, D: %s" % (np.round(PID_Output[1] * 360 / (2 * pi), 1), np.round(PID_Output[2] * 360 / (2 * pi), 1), np.round(PID_Output[3] * 360 / (2 * pi), 1)), False, Black)
         # Create surface with text displaying the control signal in degrees.
-        ControlSignalTxt = Font1.render("Saturation: %s, Control Signal: %s, Theta: %s" % (Saturation, np.round(ControlSignal * 360 / (2 * pi), 1), np.round(Theta * 360 / (2 * pi), 1)), False, Black)
+        ControlSignalTxt = Font1.render("Saturation: %s, Control Signal: %s" % (PID_Controller1.Saturation, np.round(Theta * 360 / (2 * pi), 1)), False, Black)
 
         # Update graphics. Could optimise.
         Screen.fill(White)
@@ -208,3 +141,7 @@ def PID_sim():
         #print("{:.0f}ms".format(TimeStep * 1000))
 
     pygame.quit()
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
