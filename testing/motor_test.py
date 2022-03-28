faf75e21fbd45a4cd694159d622313c1311cea3b
Justin Yeung <justinyeung@eduroam-237-173.nomadic.bris.ac.uk>
2022-03-28 16:31:44 +0100

    Minor changes.

--- testing/motor_test.py
+++ testing/motor_test.py
@@ -6,18 +6,27 @@ This file contains functions used to test and calibrate the motor control functi
 # Import modules.
 import numpy as np
 from math import pi
-from time import sleep
+from time import sleep, perf_counter
 
 # Import functions.
-from motor_control.motor_control import motor_angle, motor_reset
+from motor_control.motor_control import motor_angle, motor_angle2, motor_reset
 
 def test1():
     motor_reset()
+    LastTime = perf_counter()
     motor_angle(np.array([-pi / 4, -pi / 4]))
+    TimeStep = perf_counter() - LastTime
+    print(round(TimeStep * 1000, 2))
     sleep(1)
+    LastTime = perf_counter()
     motor_angle(np.array([0, 0]))
+    TimeStep = perf_counter() - LastTime
+    print(round(TimeStep * 1000, 2))
     sleep(1)
+    LastTime = perf_counter()
     motor_angle(np.array([pi / 4, pi / 4]))
+    TimeStep = perf_counter() - LastTime
+    print(round(TimeStep * 1000, 2))
 
 def test2():
     motor_reset()
