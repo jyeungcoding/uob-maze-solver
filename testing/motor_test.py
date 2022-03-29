c16984d4c27b53bfd0fedd75edac0c780f7e5adb
Justin Yeung <justinyeung@Justins-MacBook-Pro.local>
2022-03-29 17:18:50 +0100

    Added an extra motor control file using a downloaded module as a backup.

--- testing/motor_test.py
+++ testing/motor_test.py
@@ -10,11 +10,12 @@ from time import sleep, perf_counter
 
 # Import functions.
 from motor_control.motor_control import motor_angle, motor_reset
+from motor_control.motor_control_2 import MotorController
 
 def test1():
     motor_reset()
     LastTime = perf_counter()
-    motor_angle(np.array([-pi / 4, -pi / 4]))
+    motor_angle(np.array([-pi / 3, -pi / 3]))
     TimeStep = perf_counter() - LastTime
     print(round(TimeStep * 1000, 2))
     sleep(1)
@@ -24,23 +25,35 @@ def test1():
     print(round(TimeStep * 1000, 2))
     sleep(1)
     LastTime = perf_counter()
-    motor_angle(np.array([pi / 4, pi / 4]))
+    motor_angle(np.array([pi / 3, pi / 3]))
     TimeStep = perf_counter() - LastTime
     print(round(TimeStep * 1000, 2))
 
 def test2():
+    MotorController_ = MotorController()
+    MotorController_.start()
+    LastTime = perf_counter()
+    MotorController_.change_angle(np.array([-pi / 3, -pi / 3]))
+    TimeStep = perf_counter() - LastTime
+    print(round(TimeStep * 1000, 2))
+    sleep(1)
+    LastTime = perf_counter()
+    MotorController_.change_angle(np.array([0, 0]))
+    TimeStep = perf_counter() - LastTime
+    print(round(TimeStep * 1000, 2))
+    sleep(1)
+    LastTime = perf_counter()
+    MotorController_.change_angle(np.array([pi / 3, pi / 3]))
+    TimeStep = perf_counter() - LastTime
+    print(round(TimeStep * 1000, 2))
+    sleep(5)
+    MotorController_.stop()
+
+def test3():
     motor_reset()
     for x in range(-33, 33):
         motor_angle(np.array([0.01 * x * pi, 0.01 * x * pi]))
         sleep(0.1)
-
-def test3():
-    motor_reset()
-    motor_angle(np.array([0, 0]))
-    sleep(2)
-    motor_angle(np.array([0.01 * pi, 0.0]))
-    sleep(0.5)
-    motor_angle(np.array([0, 0]))
 
 if __name__ == "__main__":
     import doctest
