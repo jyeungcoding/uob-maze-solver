0b1b974242e829fe0c40bfe9514b5445be6a6bc8
Justin Yeung <justinyeung@Justins-MacBook-Pro.local>
2022-03-29 13:44:35 +0100

    Added maze 1 checkpoints.

--- mazes.py
+++ mazes.py
@@ -6,14 +6,30 @@ This file stores the Maze objects for Maze 1,2 and 3.
 # Import modules.
 import numpy as np
 
-from objects import Maze
+from objects import Maze, Ball, Checkpoint
 
 # Delete this line when you're done:
 from simulation.objects import SandboxMaze, SimpleMaze, CircleMaze
 
-# Please replace these with the actual mazes.
+file = open("maze1points.txt", 'r')
+text = file.read()
+file.close()
 
-Maze1 = SandboxMaze
+textpoints = text.split("\n")
+
+textpoints = [textpoint.split(",") for textpoint in textpoints]
+
+points = [np.array([28.75 + int(point[0]), 28.25 + int(point[1])]) for point in textpoints]
+
+Ball1 = Ball(np.array([28.75 + 123, 28.25 + 10]))
+
+Maze1 = Maze(Ball1, [], [], [])
+
+for point in points:
+    Checkpoint_ = Checkpoint(point)
+    Maze1.Checkpoints.append(Checkpoint_)
+
+# Please replace these with the actual mazes.
 Maze2 = SimpleMaze
 Maze3 = CircleMaze
 
