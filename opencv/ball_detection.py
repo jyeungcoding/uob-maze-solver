38738c6a66e072cdd74eb0bf32a8f610ef6f99c5
Justin Yeung <justinyeung@Justins-MacBook-Pro.local>
2022-04-01 19:55:12 +0100

    Testing.

--- opencv/ball_detection.py
+++ opencv/ball_detection.py
@@ -14,22 +14,35 @@ ControlRate = 20 # Frequency of control loop. [Hz]
 # Initialise the camera.
 # Set sensor mode to 4. Refer to Raspicam documentation. Size: 1640x1232.
 # Sets the framerate at which video-port based image captures will run.
-Camera = PiCamera(sensor_mode = 4, framerate = ControlRate)
+Camera = PiCamera(sensor_mode = 4, framerate = ControlRate) """ See if fixing the camera settings improves performance. """
 # Create an object containing an array in the correct openCV format to store each frame. The camera arg just saves a reference to the camera.
 Capture = PiRGBArray(Camera, size=(640, 480)) # Size should be the same as the size of the input frames.
 sleep(0.2) # Wait for the camera to warm up.
 # Outputs an infinite iterable that inserts the next frame into Capture as the output every time you call it.
 # Change frame format to BGR (for openCV) and resize it to (640, 480) for faster processing. Use video port for faster frame capture.
-Frames = camera.capture_continuous(Capture, format = "bgr", resize = (640, 480), use_video_port = True)
+Frames = Camera.capture_continuous(Capture, format = "bgr", resize = (640, 480), use_video_port = True)
 """ PICAMERA INITIALISATION END """
 
 while True:
+
+	""" IMAGE CAPTURE START """
 	try:
 		Frame = next(Frames) # If there is a new frame, grab it.
 		Img = Frame.array # Store the array from the frame object.
-		Img2 = Capture.array
-		cv2.imshow("Frame", Img2)
-		Capture.truncate(0) # Clear the
+		#Img = Capture.array # The line above is equivalent to this since next(Frames) returns Capture filled with the current frame.
+		Capture.truncate(0) # Clear Capture so the next frame can be inserted.
+		ControlOn = True
 	except StopIteration:
+		ControlOn = False
 		break
-	cv2.waitKey(10)
+	""" IMAGE CAPTURE END """
+
+	""" IMAGE DETECTION START """
+	if ControlOn = True:
+
+	""" IMAGE DETECTION END """
+
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+Camera.close()
