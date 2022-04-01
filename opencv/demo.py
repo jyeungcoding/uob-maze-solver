afa823e8c8585ae512fb96e84b41656b2559adac
Justin Yeung <justinyeung@Justins-MacBook-Pro.local>
2022-04-01 12:31:43 +0100

    Testing.

--- opencv/demo.py
+++ opencv/demo.py
@@ -2,14 +2,13 @@
 
 from picamera.array import PiRGBArray
 from picamera import PiCamera
+import time
 import cv2
 import numpy as np
 
 # initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
+camera = PiCamera(sensor_mode=4, framerate=20)
+rawCapture = PiRGBArray(camera, size=(1640, 1232))
 # allow the camera to warmup
 time.sleep(0.1)
 # capture frames from the camera
