c558f78b6a78f2cb469c8742a5267b1a522d0c77
Justin Yeung <justinyeung@Justins-MacBook-Pro.local>
2022-04-01 12:57:25 +0100

    Testing

--- opencv/demo2.py
+++ opencv/demo2.py
@@ -8,21 +8,21 @@ import numpy as np
 
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera(sensor_mode=4)
-camera.framerate=20 # Sets the framerate at which video-port based image captures will run.
 # It seems that the camera arg doesn't actually do anything, just saves a reference to the camera.
 rawCapture = PiRGBArray(camera, size=(640, 480))
 # allow the camera to warmup
 time.sleep(0.1)
 # capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", resize=(640, 480), use_video_port=True):
+while True:
+	camera.capture(rawCapture, format="bgr", resize=(640, 480))
 	# grab the raw NumPy array representing the image, then initialize the timestamp
 	# and occupied/unoccupied text
 	image = frame.array
 	# show the frame
 	cv2.imshow("Frame", image)
-	key = cv2.waitKey(1) & 0xFF
 	# clear the stream in preparation for the next frame
 	rawCapture.truncate(0)
+	cv2.waitKey(50)
 	# if the `q` key was pressed, break from the loop
 	if key == ord("q"):
 		break
