4b1e7c66773e9bbde90f255821a9690406218518
Justin Yeung <justinyeung@Justins-MacBook-Pro.local>
2022-04-05 21:10:16 +0100

    Minor changes.

--- testing/image_detection_test.py
+++ testing/image_detection_test.py
@@ -152,7 +152,6 @@ def image_detection_test():
             """ IMAGE PROCESSOR INITIALISATION START """
             ImageProcessor_ = ImageProcessor(perf_counter(), MazeSize, HSVLimitsBlue, HSVLimitsGreen) # Initialise image processor.
 
-            Capture.truncate(0) # Clear Capture so the next frame can be inserted.
             Frame = next(Frames) # If there is a new frame, grab it.
             Image = Frame.array # Store the array from the frame object.
             ActiveMaze.Ball.Active, ActiveMaze.Ball.S = ImageProcessor_.update(perf_counter(), Image) # Initialise ball position.
